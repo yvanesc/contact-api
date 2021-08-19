66eeb12e324dde3e06d7ff66258158d70da07d75
yvanesc <yvanesc@users.noreply.github.com>
2021-08-19 18:50:09 +0200

    basic CRUD
    CRUD ok

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # get 'contacts/index'
+  # get 'contacts/show'
+  # get 'contacts/new'
+  # get 'contacts/edit'
+  resources :contacts
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Contact.create(firstname: 'Yvan', lastname: 'Escandon', fullname: 'Mr Yvan Escandon', address: 'Rue..', email: 'yvan@gmail.com', mobile_phone_number: '0792458194')
+Contact.create(firstname: 'Marie', lastname: 'Muller', fullname: 'Mme Marie Muller', address: 'Chemin..', email: 'marie@gmail.com', mobile_phone_number: '078')
